bad5baf23291a20394520e478bad2e650e9439a6
bakalover <kostya@shprenger.com>
2023-06-06 18:34:50 +0300

    starting mutex

--- src/mutex.rs
+++ src/mutex.rs
@@ -1,3 +1,26 @@
-pub mod core;
 pub mod bind;
+pub mod core;
 pub mod guard;
+
+#[cfg(test)]
+mod tests {
+
+    use super::{core::Mutex, bind::RefProvider};
+    use std::{thread, rc::Rc};
+    #[test]
+    pub fn sequential_locks() {
+        let mutex = Mutex::new();
+        let RefProvider = RefProvider::create_from(mutex);
+        let mut arr: Vec<i32> = Vec::new();
+        let mut handlers = vec![];
+
+        let t1 = thread::spawn(|| {
+            m.lock();
+            {
+                arr.push(0);
+            }
+            mutex.unlock();
+        });
+            handlers.push(handler);
+    }
+}
