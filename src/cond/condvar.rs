95d86741a7169fab058cf98db9af4715c752e2fc
bakalover <kostya@shprenger.com>
2023-06-15 13:54:12 +0300

    condvar core start

--- src/cond/condvar.rs
+++ src/cond/condvar.rs
@@ -1,1 +1,26 @@
-pub struct CondVar{}
+use std::sync::atomic::AtomicU32;
+use std::sync::atomic::Ordering;
+pub struct CondVar {
+    flag: AtomicU32,
+}
+
+impl CondVar {
+    pub fn wait(&self, mutex: &Mutex) {
+        let cur = self.flag.load(Ordering::Relaxed);
+        mutex.unlock();
+        while self.flag.load(Ordering::Relaxed) == cur {
+            Futex::sleep(&self.flag);
+        }
+        mutex.lock();
+    }
+
+    pub fn notify_one(&self) {
+        self.flag.fetch_add(1, Ordering::Relaxed);
+        Futex::wake_one(&self.flag);
+    }
+
+    pub fn notify_all(&self) {
+        self.flag.fetch_add(1, Ordering::Relaxed);
+        Futex::wake_all(&self.flag);
+    }
+}
