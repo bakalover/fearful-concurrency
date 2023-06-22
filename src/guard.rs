2e56a36d20e26d04dec0cbbc7c79552f0e9f5ca3
bakalover <kostya@shprenger.com>
2023-06-22 15:17:43 +0300

    guard manual toggle

--- src/guard.rs
+++ src/guard.rs
@@ -2,17 +2,34 @@
 use super::mutex::Mutex;
 pub struct Guard<'a> {
     mutex: &'a Mutex,
+    is_locked: bool,
 }
 
 impl<'a> Guard<'a> {
     pub fn new(mutex: &'a Mutex) -> Self {
         mutex.lock();
-        Guard { mutex }
+        Guard {
+            mutex,
+            is_locked: true,
+        }
+    }
+
+    pub fn lock(&mut self) {
+        assert!(!self.is_locked);
+        self.mutex.lock();
+        self.is_locked = true;
+    }
+
+    pub fn unlock(&mut self) {
+        assert!(self.is_locked);
+        self.is_locked = false;
+        self.mutex.unlock()
     }
 }
 
 impl<'a> Drop for Guard<'a> {
     fn drop(&mut self) {
+        assert!(self.is_locked);
         self.mutex.unlock();
     }
 }
